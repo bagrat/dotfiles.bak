144a507bffca840b5768dbfaaaecc3ca989d54e0
Bagrat Aznauryan <bagrat@aznauryan.org>
2015-11-04 17:36:21 +0400

    Update install script

--- install.sh
+++ install.sh
@@ -1,11 +1,47 @@
 #!/bin/bash
-# Find the absolute path to the dotfiles
 echo "Installing dotfiles"
 echo "-------------------"
 
-ABS="$(pwd)/$(dirname $0)"
+# Find the absolute path to the dotfiles
+ROOT="`cd \"$(pwd)/$(dirname $0)\"; pwd`"
 
-echo "source \"${ABS}/.bashrc\"" >> ~/.bashrc
-echo "source-file \"${ABS}/.tmux.conf\"" >> ~/.tmux.conf
-echo "source \"${ABS}/.vimrc\"" >> ~/.vimrc
+function node_exists {
+	ls "$1" &> /dev/null;
+	if [ $? == 1 ]
+	then
+		echo 0
+	else
+		echo 1
+	fi
+}
+
+function install_file {
+	SRC=$1
+	DEST=$2
+
+	if [ -z "$SRC" ] || [ -z "$DEST" ]
+	then
+		echo "Please specify source and destination"
+		return 1
+	fi
+	echo "Installing file: $DEST"
+
+	if [ `node_exists "$DEST"` == 1 ]
+	then
+		echo "    $DEST already exists, backing up..."
+		mv "$DEST" "$DEST.bak.`date +%s`"
+	fi
+	ln -s "$SRC" "$DEST"
+}
+
+FILES=(".bashrc" ".bashrc.d" ".bash_completion" ".bash_completion.d" ".inputrc"
+       ".vimrc" ".vim"
+       ".tmux.conf")
+
+for FILE in ${FILES[@]}
+do
+	install_file "$ROOT/$FILE" ~/$FILE
+done
+
+echo "Done!"
 
