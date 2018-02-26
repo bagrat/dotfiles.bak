9efe84c33bef5bd6ddaac16c32dfe8782d9787d8
Bagrat Aznauryan <bagrat@aznauryan.org>
2018-02-26 14:05:22 +0400

    Point sh.bagrat.io here

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ set -x
 mkdir test
 cd test
 pwd
+
 bash <(curl -s https://raw.githubusercontent.com/bagrat/dotfiles/master/test-web.sh) > out.txt
 RESULT=`cat out.txt` && if [ "$RESULT" != 'It Works!' ]; then echo "FAIL"; exit 1; else echo "SUCCESS"; fi
 
