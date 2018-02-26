d70f19044af220624ab0b6854bb32ce3d67bf6cf
Bagrat Aznauryan <bagrat@aznauryan.org>
2018-02-26 14:32:20 +0400

    Improve web installer

--- web.sh
+++ web.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 cd
-git clone --recursive git@github.com:bagrat/dotfiles.git .dotfiles
-cd .dotfiles
-./install.sh
+git clone --recursive http://github.com/bagrat/dotfiles.git .dotfiles && cd .dotfiles && ./install.sh
